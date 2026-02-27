511bf9a817009f62814d66e8df942f0683887bf6
Karina Koldaeva <karina.mila@yandex.ru>
2026-02-28 00:13:45 +0300

    fix: add NavbarLink component, fix styles

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,13 @@
 import { Link, useLocation } from 'react-router';
+import { NavbarLink } from './NavbarLink';
 
 export const Navbar = () => {
 	const location = useLocation();
 
 	return (
-		<nav className="bg-blue-600 p-2 flex w-screen justify-between items-center">
-			<h1 className="text-white text-4xl font-bold">TEAM</h1>
-			<div className="flex justify-between text-2xl">
-				<Link
-					to="/"
-					className={`text-white px-4 py-2 rounded-md ${location.pathname === '/' ? 'font-bold cursor-default' : 'hover:bg-blue-800'}`}
-				>
-					Главная
-				</Link>
-				<Link
-					to="/favorites"
-					className={`text-white px-4 py-2 rounded-md ${location.pathname === '/favorites' ? 'font-bold cursor-default' : 'hover:bg-blue-800'}`}
-				>
-					Избранные
-				</Link>
-			</div>
+		<nav className="flex justify-between text-2xl w-3xs">
+			<NavbarLink path={'/'}>Главная</NavbarLink>
+			<NavbarLink path={'/favorites'}>Избранные</NavbarLink>
 		</nav>
 	);
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,10 @@ import { Dropdown, Navbar } from '../components';
 export const HomePage = () => {
 	return (
 		<div className="flex flex-col items-center">
-			<Navbar />
+			<div className="bg-blue-600 py-2 flex w-screen justify-between items-center">
+				<h1 className="text-white text-4xl font-bold">TEAM</h1>
+				<Navbar />
+			</div>
 			<Dropdown />
 			<Outlet />
 		</div>
