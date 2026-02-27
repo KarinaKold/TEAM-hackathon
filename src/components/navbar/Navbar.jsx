33c16862ad49b514c9eee2b75ad249078e87d5ef
Karina Koldaeva <karina.mila@yandex.ru>
2026-02-27 23:04:24 +0300

    feat: create navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,3 +1,25 @@
+import { Link, useLocation } from 'react-router';
+
 export const Navbar = () => {
-	return <div>Navbar</div>;
+	const location = useLocation();
+
+	return (
+		<nav className="bg-blue-600 p-2 flex w-screen justify-between items-center">
+			<h1 className="text-white text-4xl font-bold">TEAM</h1>
+			<div className="flex justify-between text-2xl">
+				<Link
+					to="/"
+					className={`text-white px-4 py-2 rounded-md ${location.pathname === '/' ? 'font-bold cursor-default' : 'hover:bg-blue-800'}`}
+				>
+					Главная
+				</Link>
+				<Link
+					to="/favorites"
+					className={`text-white px-4 py-2 rounded-md ${location.pathname === '/favorites' ? 'font-bold cursor-default' : 'hover:bg-blue-800'}`}
+				>
+					Избранные
+				</Link>
+			</div>
+		</nav>
+	);
 };
