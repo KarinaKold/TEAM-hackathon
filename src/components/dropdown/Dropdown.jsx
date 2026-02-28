6eddafacdcf526ea3dcb4f33160dd8bc0cfb3064
Karina Koldaeva <karina.mila@yandex.ru>
2026-02-28 14:52:49 +0300

    feat: create dropdown with team info

--- src/components/dropdown/Dropdown.jsx
+++ src/components/dropdown/Dropdown.jsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router';
+import { useState } from 'react';
+import { TeamInfo } from './TeamInfo';
 
 export const Dropdown = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsOpen((prev) => !prev);
+	};
+
 	return (
-		<div>
-			Hello team hackathon!!!
-			<Link className="text-cyan-800" to="/about">
-				About us
-			</Link>
+		<div className="relative inline-block">
+			<button
+				onClick={toggleDropdown}
+				className="pt-3 text-blue-500 focus:outline-none"
+			>
+				<svg
+					className={`w-5 h-5 duration-300 -rotate-90 ${isOpen ? 'rotate-0' : ''}`}
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path strokeWidth="5" d="M7 10l5 5 5-5H7z" />
+				</svg>
+			</button>
+			{isOpen && <TeamInfo />}
 		</div>
 	);
 };
