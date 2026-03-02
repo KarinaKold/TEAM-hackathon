b67a7a94176e1db95931fda5faacaf4e97dd8e9c
Karina Koldaeva <karina.mila@yandex.ru>
2026-03-02 20:13:59 +0300

    fix: add basename: process.env.NODE_ENV for deploy

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,18 +2,21 @@ import { createBrowserRouter } from 'react-router';
 import { FavoritesPage, HomePage, MemberPage, NotFoundPage } from '../pages';
 import { App } from '../App';
 
-export const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <App />,
-		children: [
-			{ path: '/', element: <HomePage /> },
-			{ path: '/favorites', element: <FavoritesPage /> },
-			{ path: '/member/:id', element: <MemberPage /> },
-		],
-	},
-	{
-		path: '*',
-		element: <NotFoundPage />,
-	},
-]);
+export const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			children: [
+				{ path: '/', element: <HomePage /> },
+				{ path: '/favorites', element: <FavoritesPage /> },
+				{ path: '/member/:id', element: <MemberPage /> },
+			],
+		},
+		{
+			path: '*',
+			element: <NotFoundPage />,
+		},
+	],
+	{ basename: process.env.NODE_ENV === 'production' ? '/TEAM-hackathon' : '/' },
+);
